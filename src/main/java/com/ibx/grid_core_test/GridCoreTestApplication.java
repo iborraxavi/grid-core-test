646b61df1d98fd706a498690afd8260cfe4c748f
iborraxavi <50479651+iborraxavi@users.noreply.github.com>
2023-05-26 17:14:05 +0200

    Init project

---
+++ src/main/java/com/ibx/grid_core_test/GridCoreTestApplication.java
@@ -0,0 +1,13 @@
+package com.ibx.grid_core_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridCoreTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridCoreTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibx/grid_core_test/GridCoreTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibx.grid_core_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridCoreTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
